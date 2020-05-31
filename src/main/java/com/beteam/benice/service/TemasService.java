519ffd3dc3eca9ae4dcd1327ddc07dafcb937ab1
jhonM8a <jhon.ochoa@unillanos.edu.co>
2020-05-30 23:21:01 -0500

    Se hace refac de servicio de obtener filtros

--- src/main/java/com/beteam/benice/service/TemasService.java
+++ src/main/java/com/beteam/benice/service/TemasService.java
@@ -2,8 +2,9 @@ package com.beteam.benice.service;
 
 import java.util.List;
 
-import com.beteam.benice.model.TemaResponse;
+import com.beteam.benice.domain.Tema;
 
 public interface TemasService {
-	public List<TemaResponse> getAllFilters();
+	
+	public List<Tema> getAllFilters();
 }
